4b65576b6a684a6de91074f853480047246047fe
Rocksampath1 <115952954+Rocksampath1@users.noreply.github.com>
2022-10-27 20:49:49 +0530

    Add cv website files

---
+++ s.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<dos.h>
+{
+clrscr();
+int i;
+textcolor(RED)
+textbackground(WHITE)
+for(i=1;i<=120;i++)
+{
+  printf("I LOVE YOU\n");
+  delay(200);
+}  
+getch{};
+}
